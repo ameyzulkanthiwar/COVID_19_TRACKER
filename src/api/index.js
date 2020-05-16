bea9475b9d4af74a18cd4879d2147f175836f150
Amey Zulkanthiwar <amey@patientfinder.co>
2020-05-16 14:39:25 -0500

    line chart is impliment

--- src/api/index.js
+++ src/api/index.js
@@ -20,3 +20,18 @@ export const fetchData = async () => {
         console.log("Error:", error);
     }
 };
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        return data.map(({ confirmed, deaths, reportDate: date }) => ({
+            confirmed: confirmed.total,
+            deaths: deaths.total,
+            date
+        }));
+    } catch (error) {
+        console.log("Error:", error);
+        return error;
+    }
+};
