33f31d2b4024b7bb41719efd6d6cc80388e09695
Amey Zulkanthiwar <amey@patientfinder.co>
2020-05-16 00:33:57 -0500

    created folder strucher and imported all componnet in app with cleaner approch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
 
+// importing all the component like this is a hassle
+// import Cards from "./components/Cards/Card";
+// import Chart from "./components/Chart/Chart";
+// import CountryPicker from "./components/CountryPicker/CountryPicker";
+
+// Cleaner way to import the component
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App.module.css";
+
 class App extends React.Component {
     render() {
         return (
-            <div>
+            <div className={styles.container}>
                 <h1>App</h1>
+                <Cards />
+                <Chart />
+                <CountryPicker />
             </div>
         );
     }
