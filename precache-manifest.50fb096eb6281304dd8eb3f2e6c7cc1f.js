932fd5d3d202ba8bb36448d3a063bf384efab959
Amey Zulkanthiwar <amey@patientfinder.co>
2020-05-17 09:10:39 -0500

    Updates

---
+++ precache-manifest.50fb096eb6281304dd8eb3f2e6c7cc1f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206133ca18f5e30261bcc4d3d6896475",
+    "url": "/COVID_19_TRACKER/index.html"
+  },
+  {
+    "revision": "c7434bcee62699a756ff",
+    "url": "/COVID_19_TRACKER/static/css/main.aca7c95d.chunk.css"
+  },
+  {
+    "revision": "557205fbabc121271ae6",
+    "url": "/COVID_19_TRACKER/static/js/2.54c2066f.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID_19_TRACKER/static/js/2.54c2066f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7434bcee62699a756ff",
+    "url": "/COVID_19_TRACKER/static/js/main.38de3be0.chunk.js"
+  },
+  {
+    "revision": "f7f36cc5bce9edf01716",
+    "url": "/COVID_19_TRACKER/static/js/runtime-main.8b9e9834.js"
+  },
+  {
+    "revision": "168156e593b9890a2effed2909ed11f6",
+    "url": "/COVID_19_TRACKER/static/media/404-error.168156e5.jpg"
+  },
+  {
+    "revision": "3abb128c9a9a02e8707342e399164dfe",
+    "url": "/COVID_19_TRACKER/static/media/corona_image.3abb128c.jpg"
+  }
+]);
